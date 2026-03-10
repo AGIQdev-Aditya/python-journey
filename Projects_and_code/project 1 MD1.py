669e86911d15468d36ddc35327c65c7cfca70863
AGIQdev-Aditya <agiq.dev@gmail.com>
2026-03-10 20:07:11 +0530

    Restructure repo - organized into Learnings_and_code and Projects_and_code

---
+++ Projects_and_code/project 1 MD1.py
@@ -0,0 +1,16 @@
+# okay so now this band name generator so i am not looking at the video but firstly i am building it by my
+# self according to what i got looking at this project so here i start below
+
+
+print("Welcome, here we generate band names" + " " + "do you want one?")
+a = input("which is your dream city you want to own: ")
+b = input("imagine you just got a pet, what name you want to give your pet?: ")
+print(a + " " + b)
+
+print(input("did you find it working in anyway, yes or no: "))
+
+print("obiously you foud it useless but its just for fun and to learn how to use input and print function")
+# its working brother i built above one without even looking at her lets see
+
+print("Day one completed" + ", yes it is\nso project one completed with it")
+
---
+++ Projects_and_code/project 2 MD2.py
@@ -0,0 +1,17 @@
+# tip calculator plus bill spilter
+# I am trying to build it myself just to try with looking at her doing jus by hearing what the project is here i go
+
+print("let me help you")
+u = input("Your total bill is : ₹")
+v = input("how much percent of tip you want to give : %")
+x = input("how many people are splitting the bill : ")
+
+y = round(int(u) * (int(v) / 100 + 1), 2)
+z = round(int(y) / int(x), 2)
+print(f"you total bill is : ₹{y}")
+print(f"your bill per person will be : ₹{z}")
+print("thankyou for coming")
+
+# acting here as i am working therein shop
+
+# wow done bey me not even seen her video and also didnt took long like it was fast
